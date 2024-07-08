93d57e6ba4a9a4ed8a3f975f6dcd0ed074a537bc
Gerard van der Kruijs <gerard@youngones.works>
2024-07-08 13:27:28 +0200

    Initial commit

---
+++ MainActivity.cs
@@ -0,0 +1,23 @@
+using ReactiveUI;
+
+namespace ReactiveUI_WireUpControls_Issue;
+
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class MainActivity : ReactiveActivity
+{
+
+    [WireUpResource("app_text")]
+    public TextView AppText { get; set; }
+
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Set our view from the "main" layout resource
+        SetContentView(Resource.Layout.activity_main);
+
+        this.WireUpControls();
+
+        AppText.SetText("Hello, ReactiveUI!", TextView.BufferType.Normal);
+    }
+}
